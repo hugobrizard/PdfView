03d38ec58ee738e3728d8ee3f719560450614a73
Nikita <yorl1n@users.noreply.github.com>
2016-02-25 13:37:51 +0100

    Update PdfViewController.js

--- app/controller/PdfViewController.js
+++ app/controller/PdfViewController.js
@@ -1,5 +1,7 @@
 Ext.define('PdfView.controller.PdfViewController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.pdfviewc',
-    
+    rendered: function() {
+        Ext.Msg.alert('test');
+    }
 })
