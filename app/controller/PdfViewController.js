bea464cc5c684ba7e8899fe59f02fb416bcf4636
Nikita <yorl1n@users.noreply.github.com>
2016-02-25 13:43:32 +0100

    Update PdfViewController.js

--- app/controller/PdfViewController.js
+++ app/controller/PdfViewController.js
@@ -1,6 +1,7 @@
 Ext.define('PdfView.controller.PdfViewController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.pdfviewc',
+    
     vportready: function() {
         Ext.Msg.alert('test');
     }
