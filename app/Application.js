1c793358a727a0e60fd553fef02c3c3af266ccf6
Nikita <yorl1n@users.noreply.github.com>
2016-02-25 13:19:35 +0100

    Update Application.js

--- app/Application.js
+++ app/Application.js
@@ -1,9 +1,14 @@
 Ext.define('PdfView.Application', {
     extend: 'Ext.app.Application',
     name: 'Cfd',
+    
     init: function () {
-        Ext.create('PdfView.view.Viewport', {
-            renderTo: document.getElementById('content')
+        Ext.create('Ext.container.Viewport', {
+            renderTo: document.getElementById('content'),
+            items: [{
+                xtype: 'displayfield',
+                value: 'TEST'
+            }]
         });
     }
   });
